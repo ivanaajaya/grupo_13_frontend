00b39f21faf2119f4684c15be19ae39b74352ccf
user.name like : "Ivana Ajaya <ivana.ajaya01@gmail.com>
2023-09-20 02:14:33 -0300

    listo login-register-profile

--- scripts/register.js
+++ scripts/register.js
@@ -1,39 +1,23 @@
-var fecha = getElementById("fecha_nacimiento");
-// Fecha en formato año mes día
-// var fecha = "20-09-2018"; // Por ejemplo, 18 de septiembre de 2023
 
-// Dividir la fecha en partes
-var partesFecha = fecha.split("-"); // Esto crea un array ["2023", "09", "18"]
-
-// Crear un nuevo objeto Date con las partes de la fecha
-var fechaConvertida = new Date(partesFecha[2], partesFecha[1] - 1, partesFecha[0]);
-
-// Obtener el día, mes y año en el nuevo formato
-var dia = fechaConvertida.getDate();
-var mes = fechaConvertida.getMonth() + 1; // Sumar 1 porque los meses van de 0 a 11
-var año = fechaConvertida.getFullYear();
-
-// Formatear la fecha en el nuevo formato (día mes año)
-var fechaFinal = año + " " + mes + " " + dia;
-
-console.log(fechaFinal); // Esto mostrará "18 9 2023" en la consola
-
-
-document.getElementById("Registro_Form").addEventListener("submit", function (event) {
+document.getElementById("registerForm").addEventListener("submit", function (event) {
     event.preventDefault();
-    login();
+    register();
 });
 
-function registro() {
+function register() {
     const data = {
         alias: document.getElementById("alias").value,
         nombre: document.getElementById("nombre").value,
         apellido: document.getElementById("apellido").value,
-        fecha_nacimiento: fechaFinal,
+        fecha_nacimiento: document.getElementById("fecha_nacimiento").value,
         password: document.getElementById("password").value,
+        correo_electronico: document.getElementById("correo_electronico").value,
+        fecha_registro: new Date().toISOString(), // Puedes establecer la fecha de registro aquí
+        estado_activo: true, // Por defecto, establecemos el estado a activo
+        id_rol: 1, // Debes obtener el ID del rol deseado de alguna manera
     };
 
-    fetch("http://127.0.0.1:5000/servidores", {
+    fetch("http://127.0.0.1:5000/auth/register", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -41,20 +25,19 @@ function registro() {
         body: JSON.stringify(data),
         credentials: 'include'
     })
-
     .then(response => {
         if (response.status === 200) {
-            // Redirect to profile page if login is successful
-            return response.json().then(data => {
-                window.location.href = "profile.html";
-            });
+            // Registro exitoso, redireccionar o mostrar un mensaje de éxito
+            window.location.href = "login.html"; // Redirecciona a la página de inicio de sesión
         } else {
+            // Mostrar mensaje de error
             return response.json().then(data => {
                 document.getElementById("message").innerHTML = data.message;
             });
         }
     })
     .catch(error => {
-        document.getElementById("message").innerHTML = "An error occurred.";
+        // Manejar errores de red u otros errores
+        document.getElementById("message").innerHTML = "Error al registrar usuario.";
     });
 }
